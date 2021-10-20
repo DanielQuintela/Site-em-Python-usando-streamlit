c30e89bd8d96c30fb63e2af47e54db02f1789a40
tutujovi <89170541+tutujovi@users.noreply.github.com>
2021-10-20 16:08:15 -0300

    Update cliente.py

--- LaboratorioProgramacao/models/cliente.py
+++ LaboratorioProgramacao/models/cliente.py
@@ -1,5 +1,6 @@
 class Cliente:
-    def __init__(self, nome, senha, profissao):
+    def __init__(self, nome, senha, profissao, codigo de usuario):
         self.nome = nome
         self.senha = senha
         self.profissao = profissao
+        self.codigo de usuario = codigo de usuario
