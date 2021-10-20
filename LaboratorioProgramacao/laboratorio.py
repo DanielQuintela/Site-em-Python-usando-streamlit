53d37bc4bc63fdf2899238f6d8a8a43843b5372b
Daniel <53890254+Indianfurious@users.noreply.github.com>
2021-10-19 23:00:16 -0300

    Era uma tela inicial de testes

--- LaboratorioProgramacao/laboratorio.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title('video exemplo')
-st.selectbox('Escolhan um ', ['Pilula vermelha','Pilula Azul'])
-st.sidebar.title('menu')
-st.sidebar.selectbox('Oi meu chapa', ['Para a pagina 2', 'Para a pagina 3'])
-number = st.slider("Pick a number", 0, 500)
