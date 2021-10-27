a9d2dc4c7522eab917069182d98b3468eacc8506
Daniel <53890254+Indianfurious@users.noreply.github.com>
2021-10-26 23:40:30 -0300

    Update main.py

--- LaboratorioProgramacao/main.py
+++ LaboratorioProgramacao/main.py
@@ -9,7 +9,7 @@ st.title("Tela Cadastro :3")
 with st.form(key='include_cliente'):
     input_name = st.text_input(label='Insira o seu nome')
     input_senha = st.text_input(label='Insira a senha', type="password"  )
-    input_occupation = st.selectbox('selecione sua profissão', ['Secretária])
+    input_occupation = st.selectbox('selecione sua profissão', ['Secretária'])
     input_button_enviar = st.form_submit_button("Enviar Dados")
 
 if input_button_enviar:
