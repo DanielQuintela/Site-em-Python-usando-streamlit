7b82d4115de099a014e87b0d30ce8cca278fbda8
Daniel <53890254+Indianfurious@users.noreply.github.com>
2021-11-03 21:35:53 -0300

    versão final 1.0 do main
    até agora a versão correta, só falta adicionar a confirmação de login e as próximas paginas

--- LaboratorioProgramacao/main.py
+++ LaboratorioProgramacao/main.py
@@ -4,17 +4,18 @@ import sqlite3
 con = sqlite3.connect('banco_programa.db')
 cursor = con.cursor()
 
-st.title("Tela Cadastro :3")
+st.title("Tela Cadastro")
+
 
 with st.form(key='include_cliente'):
     input_name = st.text_input(label='Insira o seu nome')
-    input_senha = st.text_input(label='Insira a senha', type="password"  )
-    input_occupation = st.selectbox('selecione sua profissão', ['Secretária'])
+    input_senha = st.text_input(label='Insira a senha', type="password")
+    input_occupation = st.selectbox('selecione sua profissão', ['Pesquisador'])
     input_button_enviar = st.form_submit_button("Enviar Dados")
 
 if input_button_enviar:
-    st.write(f'Nome{input_name}')
     cursor.execute(f"INSERT INTO cadastro VALUES ('{input_name}','{input_senha}','{input_occupation}')")
     con.commit()
 
-    
+    st.success('Adicionado com sucesso !!')
+
