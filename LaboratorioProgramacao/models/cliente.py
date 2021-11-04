90803293226b87fdf05867409a54d9a8a255e602
Daniel <53890254+Danieldev-sweet@users.noreply.github.com>
2021-11-04 19:32:40 -0300

    Não tinha uso

--- LaboratorioProgramacao/models/cliente.py
+++
@@ -1,6 +0,0 @@
-class Cliente:
-    def __init__(self, nome, senha, profissao, codigo de usuario):
-        self.nome = nome
-        self.senha = senha
-        self.profissao = profissao
-        self.codigo de usuario = codigo de usuario
