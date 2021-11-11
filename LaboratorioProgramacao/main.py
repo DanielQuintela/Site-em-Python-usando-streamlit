3d7dd24c0e82c4320e0c1542b96d3cfd4c8b12a8
Aislann <89170451+Aislann@users.noreply.github.com>
2021-11-10 21:08:53 -0300

    Update main.py

--- LaboratorioProgramacao/main.py
+++ LaboratorioProgramacao/main.py
@@ -4,48 +4,37 @@ import sqlite3
 con = sqlite3.connect('banco_programa.db')
 cursor = con.cursor()
 
-paginaSelecionada = st.sidebar.selectbox('Selecione o caminho', ['Tela de inicio','Registro funcionário','Registro Secretária'])
+paginaSelecionada = st.sidebar.selectbox('Selecione o caminho', ['Tela de inicio','Login Presidente','Login Secretária','Registro funcionário'])
 
-if paginaSelecionada == 'Registro funcionário':
+if paginaSelecionada == 'Tela de inicio':
+    st.title('Tela principal')
+
+elif paginaSelecionada == 'Registro funcionário':
     st.title("Tela Cadastro")
 
     with st.form(key='include_cliente'):
         input_name = st.text_input(label='Insira o seu nome')
         input_senha = st.text_input(label='Insira a senha', type="password")
+        input_cpf = st.text_input(label ='Insira o seu CPF')
         input_occupation = st.selectbox('selecione sua profissão', ['Pesquisador'])
         input_button_enviar = st.form_submit_button("Enviar Dados")
 
     if input_button_enviar:
-        cursor.execute(f"INSERT INTO cadastro VALUES ('{input_name}','{input_senha}','{input_occupation}')")
+        cursor.execute(f"INSERT INTO cadastro VALUES ('{input_name}','{input_senha}',{input_cpf},'{input_occupation}')")
         con.commit()
 
         st.success('Adicionado com sucesso !!')
 
-elif paginaSelecionada == 'Tela de inicio':
-    st.title('Tela principal')
+elif paginaSelecionada == 'Login Secretária':
+    st.title("Login Secretária")
 
-elif paginaSelecionada == 'Registro Secretária':
-    ''
-con = sqlite3.connect('banco_programa.db')
-cursor = con.cursor()
+    with st.form(key='include_cliente'):
+        input_button_login_Secretaria = st.form_submit_button('Login')
 
-paginaSelecionada = st.sidebar.selectbox('Selecione o caminho', ['Registro Presidente'])
 
-if paginaSelecionada == 'Registro Presidente':
-    st.title("Tela Cadastro")
+elif paginaSelecionada == 'Login Presidente':
+    st.title("Login Presidente")
 
-    with st.form(key='include_presidente'):
-        input_name = st.text_input(label='Insira o seu nome')
-        input_senha = st.text_input(label='Insira a senha', type="password")
-        input_button_enviar = st.form_submit_button("Enviar Dados")
+    with st.form(key='include_cliente'):
+        input_button_login_Presidente = st.form_submit_button('Login')
 
-    if input_button_enviar:
-        cursor.execute(f"INSERT INTO cadastro VALUES ('{input_name}','{input_senha}")
-        con.commit()
-
-        st.success('Adicionado com sucesso !!')
-
-elif paginaSelecionada == 'Tela de inicio':
-    st.title('Tela principal')
-
-elif paginaSelecionada == 'Registro Presidente':
