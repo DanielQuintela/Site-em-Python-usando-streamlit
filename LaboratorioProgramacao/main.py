b8365f89cb3f5f2ab82ddd1df01dfffe8a84c2b9
Daniel <53890254+Danieldev-sweet@users.noreply.github.com>
2021-11-26 21:42:00 -0300

    Polimento da ultima att

--- LaboratorioProgramacao/main.py
+++ LaboratorioProgramacao/main.py
@@ -5,40 +5,35 @@ import sqlite3
 con = sqlite3.connect('banco_programa.db')
 cursor = con.cursor()
 
-
 # cadastro de pesquisador, inicialmente chamado no código de user
 
 def create_usertable():
     cursor.execute(
         'CREATE TABLE IF NOT EXISTS pesquisador(nome TEXT,senha TEXT,ocupacao TEXT, cpf NUMERIC UNIQUE, situacao TEXT)')
 
-
 def add_userdata(nome, senha, ocupacao, cpf, situacao):
     cursor.execute('INSERT INTO pesquisador(nome,senha,ocupacao, cpf, situacao) VALUES (?,?,?,?,?)',
                    (nome, senha, ocupacao, cpf, situacao))
     con.commit()
-
 
 def login_user(nome, senha, situacao):
     cursor.execute('SELECT * FROM pesquisador WHERE nome = ? AND senha = ? AND situacao = ?', (nome, senha, situacao))
     data = cursor.fetchall()
     return data
 
+# cadastro do presidente
 
 def create_presdenttable():
     cursor.execute('CREATE TABLE IF NOT EXISTS presidente(nome TEXT,senha TEXT)')
-
 
 def add_presidente(nome, senha):
     cursor.execute('INSERT INTO presidente(nome,senha) VALUES (?,?)', (nome, senha))
     con.commit()
 
-
 def login_presidente(nome, senha):
     cursor.execute('SELECT * FROM presidente WHERE nome = ? AND senha = ?', (nome, senha))
     data = cursor.fetchall()
     return data
-
 
 # registro de autenticação no banco
 # de Pesquisador!! AGORA VAI
@@ -48,43 +43,48 @@ def add_data(nomex):
     cursor.execute(f'UPDATE pesquisador SET situacao = "aprovado" WHERE nome = "{nomex}" ')
     con.commit()
 
-
 # cadastro da funcionára
 
 def create_secretaria():
     cursor.execute('CREATE TABLE IF NOT EXISTS secretaria2(nome TEXT,senha TEXT)')
 
-
 def add_secretaria(nome, senha):
     cursor.execute('INSERT INTO secretaria2(nome,senha) VALUES (?,?)', (nome, senha))
     con.commit()
-
 
 def login_secretaria(nome, senha):
     cursor.execute('SELECT * FROM secretaria2 WHERE nome = ? AND senha = ?', (nome, senha))
     data = cursor.fetchall()
     return data
 
+# seleção para ver os pesquisadores no banco
 
 def view_all_titles():
     cursor.execute('SELECT DISTINCT nome FROM pesquisador')
     data = cursor.fetchall()
     return data
 
+# Exclusão de pesquisador
 
 def delete_data(resultado):
     cursor.execute('DELETE FROM pesquisador WHERE nome="{}"'.format(resultado))
     con.commit()
 
+# Criação do banco de protocolo
 def banco_protocolo():
-    cursor.execute('CREATE TABLE IF NOT EXISTS protocolos(justificativa TEXT,resumo_portugues TEXT, resumo_ingles TEXT,data_inicio TEXT,data_fim TEXT, especie TEXT, quantidade_animais TEXT, bioterio TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS protocolos(justificativa TEXT NOT NULL ,resumo_portugues TEXT, resumo_ingles TEXT,'
+                   'data_inicio TEXT,data_fim TEXT, especie TEXT NOT NULL, quantidade_animais TEXT NOT NULL, bioterio TEXT NOT NULL, situacao TEXT)')
 
-def addbanco_protocolo(input_justificativa,input_resumopt,input_resumoig,input_datainicio,input_dataterm ,input_especie ,input_qntanimal,_bioterio ):
-    cursor.execute('INSERT INTO protocolos(justificativa,resumo_portugues,resumo_ingles,data_inicio,data_fim,especie,quantidade_animais,bioterio) VALUES (?,?,?,?,?,?,?,?)', (input_justificativa,input_resumopt,input_resumoig,input_datainicio,input_dataterm ,input_especie ,input_qntanimal,_bioterio ))
+def addbanco_protocolo(input_justificativa,input_resumopt,input_resumoig,input_datainicio,input_dataterm ,input_especie ,
+                       input_qntanimal,_bioterio ):
+    cursor.execute('INSERT INTO protocolos(justificativa,resumo_portugues,resumo_ingles,data_inicio,data_fim,'
+                   'especie,quantidade_animais,bioterio) VALUES (?,?,?,?,?,?,?,?)',
+                   (input_justificativa,input_resumopt,input_resumoig,input_datainicio,input_dataterm ,input_especie ,
+                    input_qntanimal,_bioterio ))
     con.commit()
-
+# cr
 def banco_bioterio():
-    cursor.execute('CREATE TABLE IF NOT EXISTS bioterio(nome TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS bioterio(nome TEXT NOT NULL UNIQUE)')
 
 def addbanco_bioterio(cadastro_biot):
     cursor.execute('INSERT INTO bioterio VALUES (?)',(cadastro_biot,))
@@ -119,19 +119,22 @@ elif paginaSelecionada == 'Área do Pesquisador':
 
                 st.sidebar.title(f"Logado como: {nome}")
                 st.title(f'Bem vindo de Volta {nome}')
-                area_pesq =st.selectbox('Selecione o que deseja',['Emitir Protocolo','Receber protocolo'])
+                area_pesq =st.selectbox('Selecione o que deseja',['Inicio','Emitir Protocolo','Receber protocolo'])
+                if area_pesq == 'Inicio':
+                    st.title('Página inicial do pesquisador')
+                    st.text('Selecione uma opção acima para começar os trabalhos !!')
                 if area_pesq == 'Emitir Protocolo':
                     st.title('Emitir Protocolo')
                     input_justificativa = st.text_input(label='Justificativa para o uso de animais')
                     input_resumopt = st.text_input(label='Insira o resumo do trabalho em português')
                     input_resumoig = st.text_input(label='Insira o resumo do trabalho em inglês')
-                    input_datainicio = st.text_input(label='Insira a data prevista para o inicio do experimento:')
-                    input_dataterm = st.text_input(label='Insira a data prevista para o termino do experimento:')
+                    input_datainicio = st.date_input(label='Insira a data prevista para o inicio do experimento:')
+                    input_dataterm = st.date_input(label='Insira a data prevista para o termino do experimento:')
                     input_especie = st.text_input(label='Insira a especie do animal')
                     input_qntanimal = st.text_input(label='Insira a quantidade de animais')
                     unique_titles = [i[0] for i in view_all_bioterios()]
                     _bioterio = st.selectbox("Escolha o Bioterio", unique_titles)
-                    if st.button('EMITIR'):
+                    if st.button('Emitir Protocolo'):
                         banco_protocolo()
                         addbanco_protocolo(input_justificativa,input_resumopt,input_resumoig,input_datainicio,input_dataterm ,input_especie ,input_qntanimal,_bioterio )
                         st.success('Protocolo Emitido!')
@@ -218,17 +221,9 @@ elif paginaSelecionada == 'Login Presidente':
             st.sidebar.title(f"Logado como: {nome}")
             st.title(f'Bem vindo de Volta Sr {nome}')
             st.text('Presidente na Área')
-            secretaria = st.selectbox('Escolha a função', ['Inicio', 'Cadastro de Secretária','Analise de Relatorios'])
-            if secretaria == 'Cadastro de Secretária':
-                st.title('Cadastro de Secretaria')
-                input_name = st.text_input(label='Insira o seu nome')
-                input_senha = st.text_input(label='Insira sua senha', type="password")
-                if st.button("Enviar Dados"):
-                    create_secretaria()
-                    add_secretaria(input_name, input_senha)
-                    st.success(f'{input_name} Adicionada com sucesso !!')
-                    st.info("Vá para o menu de login!!")
-            elif secretaria == 'Inicio':
+            escolha = st.selectbox('Escolha a função', ['Inicio','Analise de Relatorios'])
+
+            if escolha == 'Inicio':
                 st.title('Pagina do Diretor')
                 st.subheader('Lista de Secretárias')
                 dados_secretaria = cursor.execute('SELECT nome from secretaria2')
@@ -238,6 +233,7 @@ elif paginaSelecionada == 'Login Presidente':
 
         else:
             st.warning("Usuário incorreto")
+
 
 # esse elif é para fica oculto do sistema.
 # apenas para o cadastro do presidente
@@ -251,4 +247,18 @@ elif paginaSelecionada == 'Cadastro presidente':
         create_presdenttable()
         add_presidente(input_name, input_senha)
         st.success('Adicionado com sucesso !!')
+        st.info("Vá para o menu de login!!")
+
+
+# esse elif é para fica oculto do sistema.
+# apenas para o cadastro da secretária
+
+elif paginaSelecionada == 'Cadastro de Secretária':
+    st.title('Cadastro de Secretaria')
+    input_name = st.text_input(label='Insira o seu nome')
+    input_senha = st.text_input(label='Insira sua senha', type="password")
+    if st.button("Enviar Dados"):
+        create_secretaria()
+        add_secretaria(input_name, input_senha)
+        st.success(f'{input_name} Adicionada com sucesso !!')
         st.info("Vá para o menu de login!!")
